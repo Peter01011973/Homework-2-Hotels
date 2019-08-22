5220a784e81c95c8438fa61fe8c9294d38e8e915
Peter <petr_adamovich@i.ua>
2019-08-22 14:45:34 +0300

    final version

--- src/app/frofile/frofile.component.spec.ts
+++ src/app/frofile/frofile.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FrofileComponent } from './frofile.component';
+// import { FrofileComponent } from './frofile.component';
 
-describe('FrofileComponent', () => {
-  let component: FrofileComponent;
-  let fixture: ComponentFixture<FrofileComponent>;
+// describe('FrofileComponent', () => {
+//   let component: FrofileComponent;
+//   let fixture: ComponentFixture<FrofileComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FrofileComponent ]
-    })
-    .compileComponents();
-  }));
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ FrofileComponent ]
+//     })
+//     .compileComponents();
+//   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FrofileComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(FrofileComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
